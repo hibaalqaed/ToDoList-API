bb5226ce1ff841191816885c3d5b705b110c5f26
HIBA ALQAED <hibaalqaed@Hibas-New-MacBook-Air.local>
2020-11-24 17:33:44 +0300

    Updates

--- data.js
+++ data.js
@@ -3,28 +3,33 @@ const data = [
     id: 1,
     name: "Finish the feature",
     status: true,
+    // priority: [low, medium, high],
   },
 
   {
     id: 2,
     name: "Clean my room",
     status: true,
+    // priority: [low, medium, high],
   },
 
   {
     id: 3,
     name: "Study",
     status: true,
+    // priority: [low, medium, high],
   },
   {
     id: 4,
     name: "Exercise",
     status: true,
+    // priority: [low, medium, high],
   },
   {
     id: 5,
     name: "Reply to e-mails",
     status: true,
+    // priority: [low, medium, high],
   },
 ];
 
